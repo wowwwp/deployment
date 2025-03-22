7d3a0070210b5f2504f3fb7aa3cd463aa2b4c235
pooja <e0773178@u.nus.edu>
2025-03-22 19:58:03 +0800

    Index cv-valid.csv into elasticsearch cluster

--- elastic-backend/cv-index.py
+++ elastic-backend/cv-index.py
@@ -1,19 +1,21 @@
 from elasticsearch import Elasticsearch, helpers
 import pandas as pd
 
-client = Elasticsearch("http://localhost:9200")
+client = Elasticsearch("http://3.104.109.157:9200")
 
 # Create an index if it doesnt exist
 if not client.indices.exists(index="cv-transcriptions"):
     client.indices.create(index="cv-transcriptions")
 
 # Read the csv file
-df = pd.read_csv('../asr/cv-valid-dev.csv')
+df = pd.read_csv('./cv-valid-dev.csv')
 df.fillna("", inplace=True)
 
 # Convert the dataframe to a dictionary
 data = [
-    {"_index": "cv-index", "_id": record["filename"], "_source": record}
+    {"_index": "cv-transcriptions",
+     "_type": "doc",
+        "_id": record["filename"], "_source": record}
     for record in df.to_dict(orient="records")
 ]
 
